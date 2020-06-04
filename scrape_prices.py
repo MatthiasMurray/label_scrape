58ee7ccfcf40c251db834f94902ec5c3f2eb6ec2
MatthiasMurray <matthias.murray@ncf.edu>
2020-06-04 11:05:41 -0400

    Finished first draft of price scraping module

--- scrape_prices.py
+++ scrape_prices.py
@@ -53,4 +53,57 @@ def quotepage(tick):
       company_search_box.send_keys(tick.upper())
     except:
       return 'ERROR: FAIL TO SEARCH TICKER. CHECK SYNTAX OR CONNECTION.'
+  
+  #IF SUCCESSFUL, NO NEED TO SElECT A QUOTE.
+  #CURRENTLY FAILS IF NOT AUTOMATICALLY DIRECTED TO A QUOTE
+  try:
+    search_but = driver.find_element_by_id('header-desktop-search-button')
+    search_but.click()
+  except:
+    time.sleep(1)
+    try:
+      search_but = driver.find_element_by_id('header-desktop-search-button')
+      search_but.click()
+      quote_url = driver.getCurrentUrl()
+      if not ('quote' in quote_url):
+        return 'ERROR: POSSIBLE INVALID QUOTE. CHECK CONNECTION OR SYNTAX -- YAHOO DID NOT AUTOMATICALLY RECOGNIZE QUOTE'
+    except:
+      return 'ERROR: FAILED TO CLICK SEARCH BUTTON. CHECK CONNECTION OR SYNTAX.'
+
+  return driver
+
+##PARTS 4-6
+def setdates(dr,start='2015-01-01',end='2020-06-03'):
+  
+  #PART 4: Enter dates
+  try:
+    startdatebox = dr.find_element_by_name('startDate')
+    startdatebox.send_keys(start)
+  except:
+    return 'ERROR: FAILED TO SET START DATE'
+  
+  try:
+    enddatebox = dr.find_element_by_name('endDate')
+    enddatebox.send_keys(end)
+  except:
+    return 'ERROR: FAILED TO SET END DATE'
+
+
+  #PART 5: LAZY APPROACH: SHOULD BE DAILY/HISTORICAL BY DEFAULT. SKIP TO PART 6...
+  
+  #PART 6: CLICK APPLY
+  try:
+    apply_but = driver.find_elements_by_tag_name('button')[5]
+    apply_but.click()
+  except:
+    return 'ERROR: FAILURE TO APPLY DATES/PREFS. CHECK CONNECTION/SYNTAX'
+  
+  time.sleep(1)
     
+
+  #PART 7: CLICK DOWNLOAD
+  try:
+    downl_but = driver.find_elements_by_tag_name('svg')
+    downl_but.click()
+  except:
+    return 'ERROR: FAILURE TO DOWNLOAD'
